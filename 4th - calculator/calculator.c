6e529a1a4b1ec7288ffedad801ba5c72a120833b
ImmortalTurtle <immortalsuperturtle@gmail.com>
2016-11-20 01:05:17 +0300

    Added summarizing positive values

--- 4th - calculator/calculator.c
+++ 4th - calculator/calculator.c
@@ -12,9 +12,10 @@
 #define WRONG_NUMBER_SIGN_STRING "Wrong number's sign was entered."
 
 int isDigit(char ch);
+BigNum* sum(BigNum *a, BigNum *b);
 
 int main() {
-	BigNum *a = NULL;
+	BigNum *a = NULL, *b = NULL;
 
 	//TODO: get input into another function
 	char c;
@@ -35,7 +36,7 @@ int main() {
 			} else if(isDigit(c)) {
 				if(list == NULL) 
 					list = createLinkedList();
-				pushFront(list, c);
+				pushFront(list, (c - '0'));
 			} else if(c == '\0' || c == '\n' || c == QUIT) {
 				continue; //mb not the best idea
 			} else {
@@ -61,7 +62,10 @@ int main() {
 			}
 
 			// printf("%d", sign);
-			a = createBigNum(list, sign);
+			if(a == NULL) 
+				a = createBigNum(list, sign);
+			else
+				b = createBigNum(list, sign);
 		} else {
 			switch(c) {
 				case '+':
@@ -77,11 +81,63 @@ int main() {
 	while(c != QUIT);
 
 	printBigNum(a);
-
+	printBigNum(b);
+	if(a != NULL && b != NULL) 
+		printBigNum(sum(a,b));
 	return 0;
 }
 
 //returns 1 if digit and 0 if not
 int isDigit(char ch) {
 	return 1 ? (ch <= '9' && ch >= '0') : 0;
+}
+
+BigNum* sum(BigNum* a, BigNum *b) {
+	BigNum* sum = createBigNumEmpty();
+	Node *aDig = a->digs->head;
+	Node *bDig = b->digs->head;
+
+	//add subtraction when one < 0
+	char tempSum = 0;
+	size_t index = 0;
+	while(aDig != NULL || bDig != NULL) {
+		tempSum = 0;
+		if(aDig != NULL) 
+			tempSum += aDig->value;
+		if(bDig != NULL) 
+			tempSum += bDig->value;
+
+		//if we have 1 in current rank from last operation
+		if(index == sum->digs->size - 1) { 
+			if(tempSum > 9 - sum->digs->tail->value) {
+				sum->digs->tail->value += (tempSum - 10);
+				putDigit(sum, 1);
+			} else {
+				sum->digs->tail->value += tempSum;
+			}
+		} else {
+			if(tempSum > 9) {
+				putDigit(sum, tempSum - 10);
+				putDigit(sum, 1);
+			} else {
+				putDigit(sum, tempSum);
+			}
+		}
+		
+		
+		if(aDig != NULL)
+			aDig = aDig->next;
+		if(bDig != NULL)
+			bDig = bDig->next;
+		index++;
+		
+	}
+	
+	if(sum->digs->size == 0) 
+		putDigit(sum, 0); //FIXME: is it correct? when is size 0
+	return sum;
+}
+
+char maxChar(char a, char b) {
+	return a ? a > b : b;
 }
