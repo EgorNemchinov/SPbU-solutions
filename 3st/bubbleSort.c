976578f2a8643198551d9483b9b6d0c587d5a5db
ImmortalTurtle <immortalsuperturtle@gmail.com>
2016-11-23 01:49:51 +0300

    Changed bubbleSort's input way

--- 3st/bubbleSort.c
+++ 3st/bubbleSort.c
@@ -1,63 +1,51 @@
 #include <stdio.h>
-#include <string.h> 
 #include <stdlib.h> 
-#include "compare.c"
-#define MAX_STRING_LENGTH 256
-#define MAX_STRINGS_AMOUNT 100
+#include "tools.h"
 
-int main() {
-	int n, i, swapped, j, k;
-	FILE *fin, *fout;
-	fin = fopen("input.txt", "r");
-	fout = fopen("output.txt", "w");
-	// printf("Enter amount of strings:\n");
-	fscanf(fin, "%i\n", &n);
-	// printf("Enter strings one by one:\n");
+void bubbleSort(char ** strings, int n);
+
+int main(int argc, char ** argv) {
+	int n, i;
 	char **strings;
-	strings = malloc(MAX_STRINGS_AMOUNT * sizeof(char*));
+	FILE *fin;
 
-	for (i = 0; i < MAX_STRINGS_AMOUNT; ++i)
-	{
-		strings[i] = malloc((MAX_STRING_LENGTH + 1) * sizeof(char));
+	if(argc != 3) {
+		fprintf(stderr, "Invalid arguments. Enter amount of strings and name of the input file.\n");
+		exit(1);
 	}
+
+	n = atoi(argv[1]);
+	if (!(fin = fopen(argv[2], "r"))) {
+        perror(argv[2]);
+        exit(1);
+    }
+	
+	strings = (char**) malloc(n * sizeof(char*));
+	readInput(fin, strings, n);
+
+	bubbleSort(strings, n);
 
 	for (i = 0; i < n; ++i)
 	{
-		fgets(strings[i], sizeof(strings[i]), fin);
-		strings[i][strcspn(strings[i], "\n")] = 0;
 		puts(strings[i]);
+		free(strings[i]);
 	}
+	free(strings);
+}
+
+void bubbleSort(char ** strings, int n) {
+	int i, swapped, j;
 	for (i = 0; i < n - 1; i++)
 	{
 		swapped = 0;
 		for (j = 0; j < n - i - 1; j++) {
 		
 			if(compareStrings(strings[j], strings[j + 1]) == 1) {
-				char *temp = strings[j];
-				strings[j] = strings[j+1];
-				strings[j+1] = temp;
-				/*for (k = 0; k < MAX_STRING_LENGTH; ++k) //меняем строки местами
-				{
-						char temp = strings[j][k];
-						strings[j][k] = strings[j+1][k];
-						strings[j+1][k] = temp;
-				} */
+				swap(strings, j, j+1);
 				swapped = 1;
 			}
 		}
 		if(swapped == 0) 
 			continue;
 	}
-	puts("Finished");
-	for (i = 0; i < n; ++i)
-	{
-		fprintf(fout, "%d - %s\n", i, strings[i]);
-	}
-
-	for (i = 0; i < MAX_STRINGS_AMOUNT; ++i)
-	{
-		free(strings[i]);
-	}
-	fclose(fin);
-	fclose(fout);
 }
