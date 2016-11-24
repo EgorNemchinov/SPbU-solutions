52fc394087aea39e4456439584670563f917f046
ImmortalTurtle <immortalsuperturtle@gmail.com>
2016-11-24 22:19:14 +0300

    Implemented inputting numbers into stack

--- 4th - calculator/calculator.c
+++ 4th - calculator/calculator.c
@@ -1,12 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include "big_num.c"
+// #include "big_num.c"
+#include "stack.c"
 #define QUIT '='
+#define BASE 10
 #define POS 1
 #define NEG 2
-#define INVALID_SYMBOL 1
-#define MANY_OPERATORS 2
-#define WRONG_NUMBER_SIGN 3
 #define INVALID_SYMBOL_STRING "Invalid symbol was entered."
 #define MANY_OPERATORS_STRING "Too many operators."
 #define WRONG_NUMBER_SIGN_STRING "Wrong number's sign was entered."
@@ -16,18 +15,25 @@ BigNum* sum(BigNum *a, BigNum *b);
 BigNum* sumPos(BigNum* a, BigNum *b);
 BigNum* sub(BigNum* a, BigNum *b);
 BigNum* subPos(BigNum* a, BigNum *b);
+BigNum* mul(BigNum *a, BigNum *b);
+BigNum* mulPos(BigNum *a, BigNum *b);
 char cmpBigNumAbs(BigNum *a, BigNum *b);
+/*void normalize(BigNum* a);
+void deleteZeros(BigNum *a);*/
 
-//FIXME:145-124=-/984
-//delete zeros from BigNum's tail.
+//TODO: split into a few files
+//TODO: make operation change first given BigNum and not create new one
+//TODO: implement stack and operations on it, free temporary BigNums
 int main() {
-	BigNum *a = NULL, *b = NULL;
+	BigNum *a = NULL, *b = NULL, *res = NULL;
+	Stack *stack = createNumStack();
 
 	//TODO: get input into another function
 	char c;
 	char lastOper = 0;
+	LinkedList *list;
 	do {
-		LinkedList *list = NULL;
+		list = NULL;
 		char oper = 0, sign = 0;
 		//reading string
 		do {
@@ -36,7 +42,7 @@ int main() {
 			if(c == '+' || c == '-' || c == '*' || c == '/') {
 				if(oper != 0) {
 					puts(MANY_OPERATORS_STRING); 
-					exit(MANY_OPERATORS);
+					exit(1);
 				} else {
 					oper = c;
 				}
@@ -48,7 +54,7 @@ int main() {
 				continue; //mb not the best idea
 			} else {
 				puts(INVALID_SYMBOL_STRING);
-				exit(INVALID_SYMBOL);
+				exit(1);
 			}
 		}
 		while(c > 0 && c != '\n' && c != QUIT ); //means not '\0' and not EOF
@@ -65,20 +71,27 @@ int main() {
 				sign = NEG;
 			else {
 				puts(WRONG_NUMBER_SIGN_STRING);
-				exit(WRONG_NUMBER_SIGN);
+				exit(1);
 			}
 
-			// printf("%d", sign);
-			if(a == NULL) 
-				a = createBigNum(list, sign);
-			else
-				b = createBigNum(list, sign);
+			stack_push(stack, createBigNum(list, sign));
 		} else {
-			switch(c) {
+			switch(oper) {
 				case '+':
-					//sum
+					b = stack_pop(stack);
+					a = stack_pop(stack);
+					res = sum(a, b);
+					stack_push(stack, res);
+					deleteBigNum(&a);
+					deleteBigNum(&b);
 					break;
 				case '-':
+					b = stack_pop(stack);
+					a = stack_pop(stack);
+					res = sub(a, b);
+					stack_push(stack, res);
+					deleteBigNum(&a);
+					deleteBigNum(&b);
 					break;
 				default:
 					break;
@@ -89,14 +102,13 @@ int main() {
 	}
 	while(c != QUIT);
 
-	// printBigNum(a);
-	// printBigNum(b);
-	if(a != NULL && b != NULL)  {
-		if(lastOper == '+') 
-			printBigNum(sum(a,b));
-		else if(lastOper == '-')
-			printBigNum(sub(a,b));
-	}
+	printBigNum(stack_peek(stack));
+
+	deleteLinkedList(&list);
+	deleteBigNum(&a);
+	deleteBigNum(&b);
+	deleteBigNum(&res);
+	deleteNumStack(&stack);
 	return 0;
 }
 
@@ -117,7 +129,6 @@ BigNum* sum(BigNum* a, BigNum *b) {
 	} else if (b->sign == NEG) { //A + (-B) == A - B
 		diff = subPos(a,b);
 	} else { //A + B
-		puts("else");
 		diff = sumPos(a, b);
 	}
 	return diff;
@@ -141,14 +152,14 @@ BigNum* sumPos(BigNum* a, BigNum *b) {
 		//if we have 1 in current rank from last operation
 		if(index == sum->digs->size - 1) { 
 			if(tempSum > 9 - sum->digs->tail->value) {
-				sum->digs->tail->value += (tempSum - 10);
+				sum->digs->tail->value += (tempSum - BASE);
 				putDigit(sum, 1);
 			} else {
 				sum->digs->tail->value += tempSum;
 			}
 		} else {
 			if(tempSum > 9) {
-				putDigit(sum, tempSum - 10);
+				putDigit(sum, tempSum - BASE);
 				putDigit(sum, 1);
 			} else {
 				putDigit(sum, tempSum);
@@ -187,7 +198,7 @@ BigNum* sub(BigNum *a, BigNum *b) {
 	return diff;
 }
 
-BigNum* subPos(BigNum *a, BigNum *b) { //TODO: all the cases.
+BigNum* subPos(BigNum *a, BigNum *b) { //TODO: check exceptions like nulls
 	BigNum* diff = createBigNumEmpty();
 	char sign;
 	Node *aDig, *bDig;
@@ -214,14 +225,14 @@ BigNum* subPos(BigNum *a, BigNum *b) { //TODO: all the cases.
 		if(index == diff->digs->size - 1) { 
 			tempDif += diff->digs->tail->value;
 			if(tempDif < 0) {
-				diff->digs->tail->value = (tempDif + 10);
+				diff->digs->tail->value = (tempDif + BASE);
 				putDigit(diff, -1);
 			} else {
 				diff->digs->tail->value = tempDif;
 			}
 		} else {
 			if(tempDif < 0) {
-				putDigit(diff, tempDif + 10); 
+				putDigit(diff, tempDif + BASE); 
 				putDigit(diff, -1);
 			} else {
 				putDigit(diff, tempDif);
@@ -236,10 +247,80 @@ BigNum* subPos(BigNum *a, BigNum *b) { //TODO: all the cases.
 	}
 
 	diff->sign = sign;
+	deleteZeros(diff);
 	
-	if(diff->digs->size == 0) 
-		putDigit(diff, 0); //FIXME: is it correct? when is size 0
 	return diff;
+}
+
+BigNum* mul(BigNum *a, BigNum *b) {
+
+	return mulPos(a, b);
+}
+
+
+BigNum* mulPos(BigNum *a, BigNum *b) {
+	BigNum* res = createBigNumEmpty();
+	Node *aNode, *bNode;
+
+	//aNode is from the bigger number
+	if(cmpBigNumAbs(a, b) >= 0) {
+		aNode = a->digs->head;
+		bNode = b->digs->head;
+	} else {
+		aNode = b->digs->head;
+		bNode = a->digs->head;
+	}
+
+	//multiplication by 0
+	if(a->digs->size <= 1) {
+		if(a->digs->head == NULL || a->digs->head == 0){
+			putDigit(res, 0);
+			return res;
+		}
+	} 
+	if(b->digs->size <= 1) {
+		if(b->digs->head == NULL || b->digs->head == 0){
+			putDigit(res, 0);
+			return res;
+		}
+	} 
+
+	char aI = 0, bI = 0;
+	Node *aWhile, *bWhile; //nodes in res, so we don't have to use getByIndex(aI+bI);
+	while(aNode != NULL) {
+		bI = 0;
+		bWhile = aWhile;
+		while(bNode != NULL) {
+			if(res->digs->size > aI + bI) {
+				// res->digs->
+				// bWhile = 
+			} else {
+				putDigit(res, (aNode->value * (bNode->value)));
+				bWhile = res->digs->tail;
+			}
+
+			bNode = bNode->next;
+			bI++;
+		}
+		if(aI == 0)
+			aWhile = res->digs->head;
+		aNode = aNode->next;
+		aI++;
+	}
+	return res;
+
+}
+
+
+BigNum* karatsuba(BigNum *a, BigNum *b) {
+	BigNum* res = createBigNumEmpty();
+	BigNum* a0 = createBigNumEmpty();
+	BigNum* a1 = createBigNumEmpty();
+	BigNum* b0 = createBigNumEmpty();
+	BigNum* b1 = createBigNumEmpty();
+
+
+
 }
 
 char maxChar(char a, char b) {
@@ -247,8 +328,8 @@ char maxChar(char a, char b) {
 }
 
 //returns 1 if a>b, 0 if a==b and -1 if a<b
-char cmpBigNumAbs(BigNum* a, BigNum *b) {//add different cases
-	char diff = 0;
+char cmpBigNumAbs(BigNum* a, BigNum *b) {
+	char diff = 0; 
 	if(a->digs->size > b->digs->size)
 		return 1;
 	if(a->digs->size < b->digs->size)
@@ -260,8 +341,8 @@ char cmpBigNumAbs(BigNum* a, BigNum *b) {//add different cases
 			return 1;
 		if(aNode->value < bNode->value)
 			return -1;
-		aNode = aNode->next;
-		bNode = bNode->next;
+		aNode = aNode->prev;
+		bNode = bNode->prev;
 	}
 	return 0;
 }
