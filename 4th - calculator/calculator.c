59917f170522d2317f3541d8f8bcd2a93f3bee74
ImmortalTurtle <immortalsuperturtle@gmail.com>
2016-12-20 10:22:35 +0300

    Adds multiplication, changes subtraction & addition

--- 4th - calculator/calculator.c
+++ 4th - calculator/calculator.c
@@ -1,11 +1,12 @@
 #include <stdlib.h>
 #include <stdio.h>
-// #include "big_num.c"
-#include "stack.c"
+#include "linked_list.h"
+#include "big_num.h"
+#include "stack.h"
 #define QUIT '='
 #define BASE 10
-#define POS 1
-#define NEG 2
+#define POS 0
+#define NEG 1
 #define INVALID_SYMBOL_STRING "Invalid symbol was entered."
 #define MANY_OPERATORS_STRING "Too many operators."
 #define WRONG_NUMBER_SIGN_STRING "Wrong number's sign was entered."
@@ -18,8 +19,6 @@ BigNum* subPos(BigNum* a, BigNum *b);
 BigNum* mul(BigNum *a, BigNum *b);
 BigNum* mulPos(BigNum *a, BigNum *b);
 char cmpBigNumAbs(BigNum *a, BigNum *b);
-/*void normalize(BigNum* a);
-void deleteZeros(BigNum *a);*/
 
 //TODO: split into a few files
 //TODO: make operation change first given BigNum and not create new one
@@ -38,7 +37,10 @@ int main() {
 		//reading string
 		do {
 			c = getchar();
-				//FIXME: get rid of copypaste
+			if(c == QUIT) {
+				quit = 1;
+				
+			} 
 			if(c == '+' || c == '-' || c == '*' || c == '/') {
 				if(oper != 0) {
 					puts(MANY_OPERATORS_STRING); 
@@ -79,20 +81,21 @@ int main() {
 			switch(oper) {
 				case '+':
 					b = stack_pop(stack);
-					a = stack_pop(stack);
-					res = sum(a, b);
-					stack_push(stack, res);
-					deleteBigNum(&a);
-					deleteBigNum(&b);
+					a = stack_peek(stack);
+					sum(a, b);
 					break;
 				case '-':
 					b = stack_pop(stack);
 					a = stack_pop(stack);
 					res = sub(a, b);
+					printBigNum(res);
 					stack_push(stack, res);
-					deleteBigNum(&a);
-					deleteBigNum(&b);
 					break;
+				case '*':
+					b = stack_pop(stack);
+					a = stack_pop(stack);
+					res = mul(a, b);
+					stack_push(stack, res);
 				default:
 					break;
 			}
@@ -100,14 +103,15 @@ int main() {
 		if(oper != 0 && list == NULL) 
 			lastOper = oper;
 	}
-	while(c != QUIT);
+	while(!quit);
+
+	printNumStack(stack);
 
 	printBigNum(stack_peek(stack));
-
 	deleteLinkedList(&list);
-	deleteBigNum(&a);
-	deleteBigNum(&b);
-	deleteBigNum(&res);
+	// deleteBigNum(&a);
+	// deleteBigNum(&b);
+	// deleteBigNum(&res);
 	deleteNumStack(&stack);
 	return 0;
 }
@@ -139,45 +143,22 @@ BigNum* sumPos(BigNum* a, BigNum *b) {
 	Node *aDig = a->digs->head;
 	Node *bDig = b->digs->head;
 
-	//add subtraction when one < 0
-	char tempSum = 0;
-	size_t index = 0;
-	while(aDig != NULL || bDig != NULL) {
-		tempSum = 0;
-		if(aDig != NULL) 
-			tempSum += aDig->value;
-		if(bDig != NULL) 
-			tempSum += bDig->value;
-
-		//if we have 1 in current rank from last operation
-		if(index == sum->digs->size - 1) { 
-			if(tempSum > 9 - sum->digs->tail->value) {
-				sum->digs->tail->value += (tempSum - BASE);
-				putDigit(sum, 1);
-			} else {
-				sum->digs->tail->value += tempSum;
-			}
-		} else {
-			if(tempSum > 9) {
-				putDigit(sum, tempSum - BASE);
-				putDigit(sum, 1);
-			} else {
-				putDigit(sum, tempSum);
-			}
-		}
-		
-		
-		if(aDig != NULL)
-			aDig = aDig->next;
-		if(bDig != NULL)
-			bDig = bDig->next;
-		index++;
-		
+	while(aDig != NULL && bDig != NULL) {
+		aDig->value += bDig->value;
+		aDig = aDig->next;
+		bDig = bDig->next;
 	}
+	while(bDig != NULL) {
+		putDigit(a, bDig->value);
+	}
+
+	normalize(a);
 	
-	if(sum->digs->size == 0) 
-		putDigit(sum, 0); //FIXME: is it correct? when is size 0
-	return sum;
+	deleteBigNum(&b);
+
+	if(a->digs->size == 0) 
+		putDigit(a, 0); //FIXME: is it correct? when is size 0
+	return a;
 }
 
 //a - b
@@ -195,66 +176,66 @@ BigNum* sub(BigNum *a, BigNum *b) {
 	} else { //A - B
 		diff = subPos(a, b);
 	}
+	printBigNum(diff);
 	return diff;
 }
 
+//TODO: subtract second from first!
 BigNum* subPos(BigNum *a, BigNum *b) { //TODO: check exceptions like nulls
-	BigNum* diff = createBigNumEmpty();
+	BigNum* diff;
 	char sign;
 	Node *aDig, *bDig;
 	if(cmpBigNumAbs(a,b) == 1) {
 		aDig = a->digs->head;
 		bDig = b->digs->head;
+		diff = a;
 		sign = POS;
 	} else {
 		aDig = b->digs->head;
 		bDig = a->digs->head;
+		diff = b;
 		sign = NEG;
 	}
 
-	char tempDif = 0;
-	size_t index = 0;
-	while(aDig != NULL || bDig != NULL) {
-		tempDif = 0;
-		if(aDig != NULL) 
-			tempDif += aDig->value;
-		if(bDig != NULL) 
-			tempDif -= bDig->value;
+	while(aDig != NULL && bDig != NULL) {
+		aDig->value -= bDig->value;
 
-		//if we have 1 in current rank from last operation
-		if(index == diff->digs->size - 1) { 
-			tempDif += diff->digs->tail->value;
-			if(tempDif < 0) {
-				diff->digs->tail->value = (tempDif + BASE);
-				putDigit(diff, -1);
-			} else {
-				diff->digs->tail->value = tempDif;
-			}
-		} else {
-			if(tempDif < 0) {
-				putDigit(diff, tempDif + BASE); 
-				putDigit(diff, -1);
-			} else {
-				putDigit(diff, tempDif);
-			}
-		}
-		
 		if(aDig != NULL)
 			aDig = aDig->next;
 		if(bDig != NULL)
 			bDig = bDig->next;
-		index++;
 	}
 
+	normalize(diff);
 	diff->sign = sign;
-	deleteZeros(diff);
+
+	if(diff == b)
+		deleteBigNum(&a);
+	else
+		deleteBigNum(&b);
 	
 	return diff;
 }
 
 BigNum* mul(BigNum *a, BigNum *b) {
+	BigNum* result;
+	if(a->sign == NEG) {
+		if(b->sign == NEG) {
+			result = mulPos(a, b);
+		} else {
+			result = mulPos(a, b);
+			result->sign = NEG;
+		}
+	} else {
+		if(b->sign == NEG) {
+			result = mulPos(a, b);
+			result->sign = NEG;	
+		} else {
+			result = mulPos(a, b);
+		}
+	}
 
-	return mulPos(a, b);
+	return result;
 }
 
 
@@ -286,63 +267,38 @@ BigNum* mulPos(BigNum *a, BigNum *b) {
 	} 
 
 	char aI = 0, bI = 0;
-	Node *aWhile, *bWhile; //nodes in res, so we don't have to use getByIndex(aI+bI);
-	while(aNode != NULL) {
+	Node *aNodeCur = aNode, *bNodeCur = bNode; 
+	//TODO: keep current nodes, so we don't have to use getByIndex(aI+bI);
+	while(aNodeCur != NULL) {
 		bI = 0;
-		bWhile = aWhile;
-		while(bNode != NULL) {
+		while(bNodeCur != NULL) {
+			char charMulResult = (aNodeCur->value)*(bNodeCur->value);
 			if(res->digs->size > aI + bI) {
-				// res->digs->
-				// bWhile = 
+				//add to existing. getByIndex takes a lot of time
+				getByIndex(res->digs, aI + bI)->value += charMulResult;
 			} else {
-				putDigit(res, (aNode->value * (bNode->value)));
-				bWhile = res->digs->tail;
+				putDigit(res, charMulResult);
 			}
 
-			bNode = bNode->next;
+			bNodeCur = bNodeCur->next;
 			bI++;
 		}
-		if(aI == 0)
-			aWhile = res->digs->head;
-		aNode = aNode->next;
+
+		bNodeCur = bNode;
+		aNodeCur = aNodeCur->next;
 		aI++;
+		normalize(res);
 	}
+
+	normalize(res);
+
+	deleteBigNum(&a);
+	deleteBigNum(&b);
+	
 	return res;
-
-}
-
-
-BigNum* karatsuba(BigNum *a, BigNum *b) {
-	BigNum* res = createBigNumEmpty();
-	BigNum* a0 = createBigNumEmpty();
-	BigNum* a1 = createBigNumEmpty();
-	BigNum* b0 = createBigNumEmpty();
-	BigNum* b1 = createBigNumEmpty();
-
-
 
 }
 
 char maxChar(char a, char b) {
 	return a ? a > b : b;
-}
-
-//returns 1 if a>b, 0 if a==b and -1 if a<b
-char cmpBigNumAbs(BigNum* a, BigNum *b) {
-	char diff = 0; 
-	if(a->digs->size > b->digs->size)
-		return 1;
-	if(a->digs->size < b->digs->size)
-		return -1;
-	Node *aNode = a->digs->tail;
-	Node *bNode = b->digs->tail;
-	while(aNode != NULL && bNode != NULL) {
-		if(aNode->value > bNode->value) 
-			return 1;
-		if(aNode->value < bNode->value)
-			return -1;
-		aNode = aNode->prev;
-		bNode = bNode->prev;
-	}
-	return 0;
 }
